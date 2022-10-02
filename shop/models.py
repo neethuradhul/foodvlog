c3562a74ea94c564c0eeaa08893ad46e6ace3c64
unknown <neethu702@gmail.com>
2022-10-02 18:07:59 +0530

    first

---
+++ shop/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.urls import reverse
+# Create your models here.
+class categ(models.Model):
+    name = models.CharField(max_length=250,unique=True)
+    slug = models.SlugField(max_length=250,unique=True)
+    class Meta:
+        ordering = ('name',)
+        verbose_name='category'
+        verbose_name_plural='categories'
+    def get_url(self):
+        return reverse('prod_cat',args=[self.slug])
+    def __str__(self):
+        return '{}'.format(self.name)
+class products(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    img = models.ImageField(upload_to='products')
+    desc = models.TextField()
+    stock = models.IntegerField()
+    price = models.IntegerField()
+    available = models.BooleanField()
+    category = models.ForeignKey(categ,on_delete=models.CASCADE)
+    class Meta:
+        ordering = ('name',)
+        verbose_name='product'
+        verbose_name_plural='products'
+    def get_url(self):
+        return reverse('details',args=[self.category.slug, self.slug])
+    def __str__(self):
+        return '{}'.format(self.name)
